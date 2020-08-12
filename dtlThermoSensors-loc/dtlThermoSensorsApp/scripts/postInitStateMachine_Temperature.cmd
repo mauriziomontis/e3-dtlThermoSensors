ea514f177135bc8884feee5b4ec0f17fd271218e
mauriziomontis <maurizio.montis@outlook.com>
2020-08-12 14:02:00 +0200

    fix state machine call

---
+++ dtlThermoSensors-loc/dtlThermoSensorsApp/scripts/postInitStateMachine_Temperature.cmd
@@ -0,0 +1,25 @@
+#==============================================================================
+# postInitStateMachine_Temperature.cmd
+#- Arguments: SECTION_NAME, TANK
+
+#-d /**
+#-d   \brief Execute DTL Temperature State Machine.
+#-d   \details Configure and start SNL State Machine devoted to manage tuners system.
+#-d   \author Maurizio Montis (INFN-LNL)
+#-d   \file
+#-d   \param SECTION_NAME Section name, i.e. DTL-010
+#-d   \param TANK Section name, i.e. 010
+#-d   \note Example call:
+#-d   \code
+#-d    ${SCRIPTEXEC} "${dtlTuner_DIR}postInitStateMachine_Temperature.cmd", "SECTION_NAME=DTL-010, TANK=010"
+#-d   \endcode
+#-d */
+
+afterInit(seq temperature${TANK}_interlock  "SECTION_NAME=${SECTION_NAME}")
+
+
+
+
+
+
+
