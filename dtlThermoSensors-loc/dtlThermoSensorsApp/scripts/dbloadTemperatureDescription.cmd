a789856dd4c1b06dada5c9783916d3fe8f2a65c1
Maurizio <maurizio.montis@outlook.com>
2020-10-22 12:57:56 +0200

    inserted sensor position records

--- dtlThermoSensors-loc/dtlThermoSensorsApp/scripts/dbloadTemperatureDescription.cmd
+++ dtlThermoSensors-loc/dtlThermoSensorsApp/scripts/dbloadTemperatureDescription.cmd
@@ -1,6 +1,6 @@
 #==============================================================================
 # dbloadTemperatureDescription.cmd
-#- Arguments: SECTION_NAME, POS_INDEX, DESCRIPTION
+#- Arguments: SECTION_NAME, POS_INDEX, DESCRIPTION, POSITION_CODE
 
 #-d /**
 #-d   \brief Load Temperature Descriptin Database
@@ -9,12 +9,12 @@
 #-d   \file
 #-d   \param SECTION_NAME Section name, i.e. DTL-010
 #-d   \param POS_INDEX Device signal mapped with this digital interface, i.e. 001
-#-d   \param DESCRIPTION Description visible in GUI  
+#-d   \param DESCRIPTION Description visible in GUI
+#-d   \param POSITION_CODE Sensor position code [0: empty location, 1: steerer position, 2: BPM position, 3: PMQ position]
 #-d   \note Example call:
 #-d   \code
-#-d    ${SCRIPTEXEC} "${dtlThermo_DIR}dbloadTemperatureDescription.cmd", "SECTION_NAME=DTL-010, POS_INDEX=001, DESCRIPTION="Example of gui info""
+#-d    ${SCRIPTEXEC} "${dtlThermo_DIR}dbloadTemperatureDescription.cmd", "SECTION_NAME=DTL-010, POS_INDEX=001, DESCRIPTION="Example of gui info", POSITION_CODE=2"
 #-d   \endcode
 #-d */
 
-dbLoadRecords(dtlTemperatureDescription.db,"PREFIX=${SECTION_NAME}, INDEX=${POS_INDEX}, DESCRIPTION=${DESCRIPTION}")
-
+dbLoadRecords(dtlTemperatureDescription.db,"PREFIX=${SECTION_NAME}, INDEX=${POS_INDEX}, DESCRIPTION=${DESCRIPTION}, POSITION_CODE=${POSITION_CODE}")
